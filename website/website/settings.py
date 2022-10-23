2376d384b65ad012916782bef6705543509624aa
h3nnn4n <git@renan.digital>
2022-10-23 10:09:56 -0300

    Update django settings

--- website/website/settings.py
+++ website/website/settings.py
@@ -23,10 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure--d-b2sp*r&a$482t!9h2ags#xiaa7kqb*67z3(crgfcapdobck"
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
