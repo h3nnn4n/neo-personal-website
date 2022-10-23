37b9a5a33b9c8f6d675daa89c5f5fcb45226bfc9
h3nnn4n <git@renan.digital>
2022-10-23 10:04:06 -0300

    Automatically setup dns entry

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -4,6 +4,11 @@ terraform {
       source = "hetznercloud/hcloud"
       version = "1.35.2"
     }
+
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
   }
 
   # https://www.terraform.io/language/settings/backends/s3
@@ -16,4 +21,8 @@ terraform {
 
 provider "hcloud" {
   token = var.hcloud_token
+}
+
+provider "aws" {
+  region = "us-east-1"
 }
