6e5506771c66503f84bf3e30343c71cf68b55c69
h3nnn4n <git@renan.digital>
2022-12-11 17:22:56 -0300

    Switch to fontawesome free 6
    weirdest version upgrade ever lol
    wanna update from frontawesome 5 to 6? Replace fontawesome-free with
    fontawesomefree haha
    
    I mean I get why this happened (bad naming in the v5)

--- website/website/settings.py
+++ website/website/settings.py
@@ -11,14 +11,11 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 import os
-import sys
 from pathlib import Path
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
-sys.modules["fontawesome_free"] = __import__("fontawesome-free")
 
 
 # Quick-start development settings - unsuitable for production
@@ -41,7 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "fontawesome_free",
+    "fontawesomefree",
     "memoize",
     "app",
 ]
