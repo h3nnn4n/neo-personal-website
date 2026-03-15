6d7ad76420ffb93d45c1c0104e6c3f23bba8394f
Renan S Silva <renan@canals.ai>
2026-03-15 18:56:59 -0300

    No more compressed static files storage

--- website/website/settings.py
+++ website/website/settings.py
@@ -133,7 +133,7 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "static"),
     os.path.join(BASE_DIR, "content"),
 ]
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 STATIC_URL = "/static/"
 
